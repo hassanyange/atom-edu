fc2fdceeef81072d09af15ee59fba509b618d972
mvikuyukk <christianmwikuyu416@gmail.com>
2026-02-04 20:46:17 +0300

    setting changes

--- core/settings.py
+++ core/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hackatom-2024-obninsk-nuclear-key'
 
 DEBUG = False
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'daphne',
